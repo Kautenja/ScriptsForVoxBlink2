70edd02becd249dc2725eb307e541f87e907ef04
Christian Kauten <Kautenja@users.noreply.github.com>
2024-11-21 19:43:51 -0700

    Update cropper_audio.py

--- cropper_audio.py
+++ cropper_audio.py
@@ -1,10 +1,12 @@
 import subprocess
-import os,cv2
+import os, cv2
 import argparse, os, random
+from tqdm import tqdm
 import multiprocessing as mp
 from multiprocessing import Manager
-import logging,sys
+import logging, sys
 from itertools import islice
+from parallel_map import parallel_map
 logging.basicConfig(level=logging.INFO)  # configure logging level to INFO
 parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -32,7 +34,7 @@ def split_dict(data, num_splits):
     random.shuffle(keys)
     split_keys = [keys[i::num_splits] for i in range(num_splits)]
     return [{k: data[k] for k in subset} for subset in split_keys]
-def frame2second(frame,fps=25):
+def frame2second(frame, fps=25):
     return frame / fps
 def prepare_timestamp(filename):
     with open(filename, 'r') as file:
@@ -42,7 +44,7 @@ def prepare_timestamp(filename):
         start_frame = start_line.strip().split("\t")[0]
         end_frame = end_line.strip().split("\t")[0]
         
-    return frame2second(int(start_frame)),frame2second(int(end_frame))
+    return frame2second(int(start_frame)), frame2second(int(end_frame))
 
 def float_to_timecode(seconds):
     hours = int(seconds // 3600)
@@ -65,47 +67,43 @@ def cut_audio(input_path, output_path, start_time, end_time):
             output_path
         ]
         # exec
-        subprocess.run(command, check=True)
+        subprocess.run(command, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         print(f"failed extraction: {e}")
 
 def crop_by_spks(spk2audios):
-    for spk, audios in spk2audios.items():
-        tsf_spk = os.path.join(args.timestamp_path,spk)
-        if not os.path.exists(tsf_spk):
-            logging.info(f"Spk dir {tsf_spk} of timestamp does not exist. Skipping...")
-            continue
-        for audio in audios:
-            tsf_audio = os.path.join(tsf_spk, audio)
-            audio_path = os.path.join(args.audio_root,spk,audio+'.wav')
-            if not os.path.exists(tsf_audio):
-                logging.info(f"audio dir {tsf_audio} of timestamp does not exist. Skipping...")
-                continue
-            if not os.path.exists(audio_path):
-                logging.info(f"audio {audio_path} does not exist. Skipping...")
-                continue
-            os.makedirs(os.path.join(args.save_dir,spk,audio),exist_ok=True)
-            for tsf in os.listdir(tsf_audio):
-                num = tsf.replace(".txt","")
-                ts = os.path.join(tsf_audio,tsf)
-                start_time, end_time= prepare_timestamp(ts)
-                save_audio_path = os.path.join(args.save_dir,spk,audio,num+".wav")
-                cut_audio(audio_path,save_audio_path,start_time,end_time)                
+    spk, audio = spk2audios
+    if isinstance(audio, list):
+        print(audio)
+    tsf_spk = os.path.join(args.timestamp_path, spk)
+    if not os.path.exists(tsf_spk):
+        logging.info(f"Spk dir {tsf_spk} of timestamp does not exist. Skipping...")
+        return
+    tsf_audio = os.path.join(tsf_spk, audio)
+    if not os.path.exists(tsf_audio):
+        logging.info(f"audio dir {tsf_audio} of timestamp does not exist. Skipping...")
+        return
+    audio_path = os.path.join(args.audio_root, spk, audio+'.wav')
+    if not os.path.exists(audio_path):
+        logging.info(f"audio {audio_path} does not exist. Skipping...")
+        return
+    os.makedirs(os.path.join(args.save_dir, spk, audio), exist_ok=True)
+    for tsf in os.listdir(tsf_audio):
+        num = tsf.replace(".txt","")
+        ts = os.path.join(tsf_audio, tsf)
+        start_time, end_time= prepare_timestamp(ts)
+        save_audio_path = os.path.join(args.save_dir, spk, audio, num+".wav")
+        cut_audio(audio_path, save_audio_path, start_time, end_time)
 
 if __name__ == '__main__':
-    print("*"*15)
-    print("* Cropping Starts *")
-    print("*"*15)
-    os.makedirs(args.save_dir,exist_ok=True)
-    os.makedirs(os.path.join(args.save_dir,"audio"),exist_ok=True)
+    os.makedirs(args.save_dir, exist_ok=True)
+    os.makedirs(os.path.join(args.save_dir,"audio"), exist_ok=True)
     spk2audios_loc = "data/spk2videos"
     assert os.path.exists(spk2audios_loc)
     spk2audios = {line.split()[0]:line.strip().split()[1:] for line in open(spk2audios_loc)}
-    workers = min(args.num_workers,len(spk2audios))
-    spk2audios_slices = split_dict(spk2audios,workers)
-    pool = mp.Pool(processes=workers)
-    pool.map(crop_by_spks, spk2audios_slices)
-    pool.close()
-    pool.join() 
-
-    
+    items = []
+    for spk, audios in tqdm(spk2audios.items(), desc='Creating parallel workload'):
+        for audio in audios:
+            items.append((spk, audio))
+    logging.info('Processing %d audio files for %d speakers', len(items), len(spk2audios))
+    parallel_map(crop_by_spks, items)
